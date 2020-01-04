8abcb35d6b19d1c97e33716f5192860fb457aa27
Jeff.A <kinesthesiac@gmail.com>
2020-01-03 20:16:25 -0700

    minor bug fixing to css and some psuedocode.

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// computer generates random number between 19 and 120
+$(Math.floor(Math.random))
+    // computer generates a random number for each crystal between 1 and 12. 
+    //these numbers remain invisisble
+
+// wire up buttons
+
+// when the user clicks a button, its value is added to the total
+
+//each time a button is clicked, a check is performed to see if the number matches or
+// exceeds the random number
+
+//if the numbers are equal the user gets a message and the wins are updated
+
+//if the user number exceeds the random number, the user gets a message and the losses
+// are updated
